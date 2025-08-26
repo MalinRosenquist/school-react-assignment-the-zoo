94bccceca4f03438c6ba5e3e69ac835df4bf367c
Malin Rosenquist <malinrosenquist@outlook.com>
2025-08-26 15:04:49 +0200

    feat: render animals with styling

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,3 +1,4 @@
+import "./animals.scss";
 import { useEffect, useState } from "react";
 import { getAllAnimals } from "../services/animalService";
 import type { Animal } from "../models/Animal";
@@ -26,12 +27,21 @@ export const Animals = () => {
   console.log(animals);
 
   return (
-    <>
+    <ul className="animal-listing">
       {animals.map((a) => (
-        <div key={a.id}>
-          <h2>{a.name}</h2>
-        </div>
+        <li key={a.id}>
+          <div className="card-wrapper">
+            <div className="card-img-wrapper">
+              <img className="card-img" src={a.imageUrl} alt={a.name} />
+            </div>
+            <div className="card-info">
+              <h2>{a.name}</h2>
+              <p>{a.shortDescription}</p>
+              <div className="card-status"></div>
+            </div>
+          </div>
+        </li>
       ))}
-    </>
+    </ul>
   );
 };
