a9da1e8fc70506100fcb8f6703433a296a4b7dd5
Malin Rosenquist <malinrosenquist@outlook.com>
2025-08-28 10:56:19 +0200

    feat: add styling to Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,27 @@
+import "./home.scss";
+import hedgehog from "../images/hedgehog.jpg";
+import bunny from "../images/bunny.jpg";
+import { Link } from "react-router";
+
 export const Home = () => {
-  return <>Startsida</>;
+  return (
+    <section className="home-section">
+      <div className="big-img-wrapper">
+        <img src={hedgehog} />
+      </div>
+      <div className="right-wrapper">
+        <div className="intro-text">
+          <p>Har du någonsin drömt om att ta hand om ett helt zoo? Här kan du mata alla dina favoritdjur och se till att de trivs – allt på ett klick!</p>
+        </div>
+        <div>
+          <Link to={"/animals"}>
+            <button>Träffa alla djuren här!</button>
+          </Link>
+        </div>
+        <div className="small-img-wrapper">
+          <img src={bunny} />
+        </div>
+      </div>
+    </section>
+  );
 };
