19c8e95b7d0c62e2bf420494534a9ecd2a3432f7
Malin Rosenquist <malinrosenquist@outlook.com>
2025-08-28 15:35:14 +0200

    refactor: move SVG to component

---
+++ src/components/graphics/WaveGraphic.tsx
@@ -0,0 +1,14 @@
+export const WaveGraphic = () => {
+  return (
+    <svg className="wave" viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+      <path
+        d="M0 40 
+         Q240 80 480 40 
+         T960 40 
+         T1440 40 
+         V100 H0 Z"
+        fill="#EFEBCE"
+      />
+    </svg>
+  );
+};
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,13 @@
 import "./header.scss";
 import { Navigation } from "../navigation/Navigation";
+import { WaveGraphic } from "../graphics/WaveGraphic";
 
 export const Header = () => {
   return (
     <header className="header">
       <h1>The Zoo</h1>
       <Navigation />
-      <svg className="wave" viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-        <path
-          d="M0 40 
-         Q240 80 480 40 
-         T960 40 
-         T1440 40 
-         V100 H0 Z"
-          fill="#EFEBCE"
-        />
-      </svg>
+      <WaveGraphic />
     </header>
   );
 };
