0f404470e05baac78a66417e5505394fbacf58ff
Malin Rosenquist <malinrosenquist@outlook.com>
2025-09-08 15:00:46 +0200

    chore: updated router and base

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,24 +5,29 @@ import { Layout } from "./pages/Layout/Layout";
 import { Animal } from "./pages/animal/Animal";
 import { Error404 } from "./pages/error/Error404";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <Error404 />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/animals",
+          element: <Animals />,
+        },
+        {
+          path: "/animal/:id",
+          element: <Animal />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    errorElement: <Error404 />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/animals",
-        element: <Animals />,
-      },
-      {
-        path: "/animal/:id",
-        element: <Animal />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.DEV ? "" : "/the-zoo-MalinRosenquist/",
+  }
+);
